7c71063ef609d7a3e74fa29fd9fc4ce71d8edc09
Jason Dsouza <jason.dsouza@ipscape.com>
2025-07-15 09:53:43 +1000

    initial commit

---
+++ frontend/vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vite.dev/config/
+export default defineConfig({
+  server: {
+    proxy: {
+      '/druid': 'http://druid-router:8888/druid'
+    }
+  },
+  plugins: [vue()],
+})
